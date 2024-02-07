ce78a3eae79347e83317fb87cd6f467aa075393b
Amos Adebisi <adebisiayodeji20@gmail.com>
2024-02-07 13:32:29 +0100

    image-section implemented

--- capuccino/lib/main.dart
+++ capuccino/lib/main.dart
@@ -23,15 +23,30 @@ class MyPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
-        title: const AppBarComponent(navIconOne: "image/arrow-left.jpg", navIconTwo: "image/Heart.jpg", titleMain: "Detail"),
-      ),);
+        title: const AppBarComponent(
+            navIconOne: "image/arrow-left.jpg",
+            navIconTwo: "image/Heart.jpg",
+            titleMain: "Detail"),
+      ),
+      body: const SingleChildScrollView(
+        child: Column(
+          children: [
+            ImageSection(image:"image/Rectangle.jpg")
+          ],
+        ),
+      ),
+    );
   }
 }
 
 class AppBarComponent extends StatelessWidget {
-  const AppBarComponent ({super.key, required this.navIconOne, required this.navIconTwo,  required this.titleMain});
+  const AppBarComponent(
+      {super.key,
+      required this.navIconOne,
+      required this.navIconTwo,
+      required this.titleMain});
 
   final String navIconOne;
   final String navIconTwo;
@@ -40,31 +55,43 @@ class AppBarComponent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 20.0),
+      padding: const EdgeInsets.fromLTRB(0.0, 40.0, 0.0, 40.0),
       child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-           Padding(
-             padding: const EdgeInsets.only(left: 20.0),
-             child: Image.asset(navIconOne,
-                     ),
-           ),   
+          Padding(
+            padding: const EdgeInsets.only(left: 15.0),
+            child: Image.asset(
+              navIconOne,
+            ),
+          ),
           Text(
-                titleMain,
-              style: const TextStyle(
-                fontFamily: "Sora",
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold, 
-              ),),
-       
-       Padding(
-         padding: const EdgeInsets.only(right: 20.0),
-         child: Image.asset(navIconTwo,
-           ),
-       ),
-      
+            titleMain,
+            style: const TextStyle(
+              fontFamily: "Sora",
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 15.0),
+            child: Image.asset(
+              navIconTwo,
+            ),
+          ),
         ],
       ),
     );
+  }
+}
+
+class ImageSection extends StatelessWidget {
+  const ImageSection({super.key, required this.image});
+
+  final String image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Image.asset(image));
   }
 }
